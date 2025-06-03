8c834df1e399543aa738e96ea0cef43e978a30ab
SophiaCG <sophia.gorgonio@gmail.com>
2025-06-02 22:27:47 -0400

    Initial commit

---
+++ Empyrean-THP/Empyrean_THPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Empyrean_THPApp.swift
+//  Empyrean-THP
+//
+//  Created by Sophia Gorgonio on 6/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Empyrean_THPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
